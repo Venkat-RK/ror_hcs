7e257647f4ac872449fac020ea72c0e027fd22bf
venkateswara.rao <venkateswara.rao@revenuemed.com>
2013-07-18 14:55:31 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorHcs::Application.config.secret_token = 'c195a759d1890a27252c06cc3593322691259d0ce8dd870dc4a7ea70d6dbc1c3f7b184587fe0003da82f2117fa52868a05708bce29a6115945d37513c1010777'
